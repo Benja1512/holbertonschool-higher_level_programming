fa0075d5125a31d3489ff22d826654ae6f0a7ec3
benjamin ontiveros <bontiveros15@gmail.com>
2023-11-24 14:10:09 -0800

    0-safe_pprint_list.py

--- python-exceptions/0-safe_pprint_list.py
+++ python-exceptions/0-safe_pprint_list.py
@@ -1,18 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    try:
-        countr = 0
-        for i in range(x):
-            print(my_list[i], end="")
-            count += 1
-        print()
-        return count
-    except IndexError:
-        print()
-        return count
-
-    # Exmaple usage:
-    
-    my_list = [1, 2, 3, 4, 5]
-    elements_printed = safe_print_list(my_list, 3)
-    print(f"Number of elements printed: {elements_printed}")
+    ret = 0
+    for i in range(x):
+        try:
+            print("{}".format(my_list[i]), end="")
+            ret += 1
+        except IndexError:
+            break
+        print("")
+        return (ret)
