c4524991e3f2d400e220e88c9b861d50c18cffe4
benjamin ontiveros <bontiveros15@gmail.com>
2023-11-26 08:55:53 -0800

    0-square.py

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """Square with private instance attribute"""
 class Square:
-    
-        def __init__(self, size):
-        """Initialize data"""
-        self.__size = size
-        """Private instantiation of size"""
+   
+
+    def __init__(self, size):
+    """Initialize data"""
+    self.__size = size
+    """Private instantiation of size"""
