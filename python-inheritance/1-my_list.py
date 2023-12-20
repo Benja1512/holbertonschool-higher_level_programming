456aa50ea7203cca352c39684ca42e6988867f5d
benjamin ontiveros <bontiveros15@gmail.com>
2023-12-20 14:39:55 -0800

    feat: function call inheritance from a class

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+class Mylist
+"""
+
+class Mylist(list):
+    """MyList class
+    """
+
+    def print_sorted(self):
+        """print list
+        """
+        print(sorted(self))
