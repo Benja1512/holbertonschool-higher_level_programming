91d0b60f2dc7c6d997dc0a645f8ce129168aeda9
benjamin ontiveros <bontiveros15@gmail.com>
2023-12-21 14:40:16 -0800

    fixed: inherited python

--- python-inheritance/11-main.py
+++ python-inheritance/11-main.py
@@ -1,7 +1,19 @@
 #!/usr/bin/python3
-Square = __import__('11-square').Square
+"""
+Square module
+"""
+Rectangle = __import__('9-rectangle').Rectangle
 
-s = Square(13)
 
-print(s)
-print(s.area())
+class Square(Rectangle):
+    """class inherits from Rectangle"""
+
+    def __init__(self, size):
+        """initialization method"""
+        self.integer_validator('size', size)
+        self.__size = size
+        super().__init__(self.__size, self.__size)
+
+    def __str__(self):
+        """print method"""
+        return "[{}] {}/{}".format(__class__.__name__, self.__size, self.__size)
