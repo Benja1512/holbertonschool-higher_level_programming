c7174e363c6e072d501a5a544709c10d99f6fb8a
benjamin ontiveros <bontiveros15@gmail.com>
2023-12-21 12:16:42 -0800

    feat: inherits

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,18 +1,26 @@
 #!/usr/bin/python3
-""" an inheritance rectangle class """
 
-def __init__(self, width, height):
-    """ declare the private variable"""
+"""Defines a base geometry class BaseGeometry."""
 
-    self.__width = int
-    self.__height = int
-    """
-        Positive Integer
-    Args:
-        width(int): validate positive integer
-        height(int): valitdate positive integer
-    """ 
-    if type(value) != int:
-        raise TypeError("{}" must be an integerr.format(name))
-    if value <= 0:
-        raise ValueError("{} must be greater than 0".format(name))
+
+class BaseGeometry:
+    """Reprsent base geometry."""
+
+    def area(self):
+        """Not yet implemented."""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Validate a parameter as an integer.
+
+        Args:
+            name (str): The name of the parameter.
+            value (int): The parameter to validate.
+        Raises:
+            TypeError: If value is not an integer.
+            ValueError: If value is <= 0.
+        """
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
