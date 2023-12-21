5f4f51c802f49250cceed888321017ce7b7c50cf
benjamin ontiveros <bontiveros15@gmail.com>
2023-12-21 12:42:12 -0800

    fixed inherit rectangle width and height

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,26 +1,16 @@
 #!/usr/bin/python3
+"""
+Class Module inherited
+"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
-"""Defines a base geometry class BaseGeometry."""
 
+class Rectangle(BaseGeometry):
+    """Rectangle a subclass"""
 
-class BaseGeometry:
-    """Reprsent base geometry."""
-
-    def area(self):
-        """Not yet implemented."""
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """Validate a parameter as an integer.
-
-        Args:
-            name (str): The name of the parameter.
-            value (int): The parameter to validate.
-        Raises:
-            TypeError: If value is not an integer.
-            ValueError: If value is <= 0.
-        """
-        if type(value) != int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+    def __init__(self, width, height):
+        """initialization method"""
+        self.integer_validator('width', width)
+        self.integer_validator('height', height)
+        self.__width = width
+        self.__height = height
