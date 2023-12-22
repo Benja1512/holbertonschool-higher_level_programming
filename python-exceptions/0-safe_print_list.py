2260f3def7f174698ed3f19eca8600a8a15ad7e8
benjamin ontiveros <bontiveros15@gmail.com>
2023-12-22 14:52:25 -0800

    feat: function print x elements of a list

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    ret = 0
-    for i in range(0, x):
+    contador = 0
+    for i in range(x):
         try:
-            print("{:d}".format(my_list[ret]), end="")
-            ret += 1
+            print("{}".format(my_list[i]), end="")
+            contador += 1
         except IndexError:
-            pass
-        print("")
-        return (ret)
+            break
+    print("")
+    return (contador)
