8b5d9de86dddf3de3bfdcf3c52206aa99bd026e4
benjamin ontiveros <bontiveros15@gmail.com>
2023-12-27 14:09:45 -0800

    fixed: comment

--- SQL_introduction/1-create_database_if_missing.sql
+++ SQL_introduction/1-create_database_if_missing.sql
@@ -1,2 +1,2 @@
--- Creates database hbtn_0c_0.
-CREATE DATABASE IF NOT EXISTS 'hbtn_0c_';
+-- Creates database hbtn_0c_0, not failing if database exists already.
+CREATE DATABASE IF NOT EXISTS hbtn_0c_;
