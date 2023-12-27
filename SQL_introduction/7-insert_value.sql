08686a5176ffcd6ce1af1a467bf9d3c53a6845fa
benjamin ontiveros <bontiveros15@gmail.com>
2023-12-27 15:55:55 -0800

    fixed: code fixed again

--- SQL_introduction/7-insert_value.sql
+++ SQL_introduction/7-insert_value.sql
@@ -1,4 +1,2 @@
--- Inserts new row in `first_table`
--- id = 89, name = Holberton School
-INSERT INTO first_table(id, name)
-VALUES ('89','Holberton School');
+-- a script that inserts a new row in the table first_table (database hbtn_0c_0) in your MySQL server.
+INSERT INTO `first_table` (`id`, `name`) VALUES (89, "Best School");
