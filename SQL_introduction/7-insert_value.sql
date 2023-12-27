3e75ebb3101cc5eb0cdaf3840bfd2d713b939f05
benjamin ontiveros <bontiveros15@gmail.com>
2023-12-27 15:48:36 -0800

    fixed: organised

--- SQL_introduction/7-insert_value.sql
+++ SQL_introduction/7-insert_value.sql
@@ -1,2 +1,3 @@
 -- write a script that inserts a new row in table first_Table from database hbnt_0c_0 from my sqlserver.
-INSERT INTO first_table(id, name) VALUES ('89', 'Holberton School');
+INSERT INTO first_table(id, name)
+	VALUES ('89', 'Holberton School');
