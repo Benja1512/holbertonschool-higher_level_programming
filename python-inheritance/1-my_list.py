ffdb3973bb6f73d408c59cb879e9016d20df8c07
benjamin ontiveros <bontiveros15@gmail.com>
2023-12-29 09:05:27 -0800

    fixed: code ok

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-"""Module for MyList class"""
+"""Defines an inherited list class MyList."""
 
 
 class MyList(list):
-    """Inherits from 'list'"""
+    """Implements sorted printing for the built-in list class."""
+
     def print_sorted(self):
-        """Method to print sorted list"""
+        """Print a list in sorted ascending order."""
         print(sorted(self))
